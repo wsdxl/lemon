a99aeb9ce74ce472d449e06df361a22a071fcecb
wsdxl <dxl20@163.com>
2019-10-17 09:10:23 +0800

    lemon

---
+++ python_bases/excise_191017/base1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time      : 2019/10/17 8:57
+# @Author    : XiaoLei
+# @Email     : 506615839@qq.com
+# @File      : base1.py
+# @Software  : PyCharm
+
+'''
+注释
+'''
+
+#print('这个是单行注释'）
+#下面是多行注释
+'''
+啦啦啦
+啦啦啦
+lalala
+'''
+
